43e7241a540f877f8b93c7186d554bf26968819c
Ernesto Hernandez Bernal <neto87878@gmail.com>
2024-11-26 20:30:31 -0600

    archivo corregido

--- pruebas/prueba2.py
+++ pruebas/prueba2.py
@@ -5,9 +5,10 @@ def greet(name:str)->str:
 
 def main():
   """Main function."""
-  name="World"  # Sin espacio alrededor del operador de asignación
+  name = "World"  # Sin espacio alrededor del operador de asignación
   message=greet(name)
   print(message)      # Indentación incorrecta
 
 
-if __name__=="__main__":main()  # Todo en una sola línea
+if __name__ == "__main__":
+    main()  # Todo en una sola línea
